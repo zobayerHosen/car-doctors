73f7363a1421cbb6c09f79135338389683f01320
zobayerHosen <zobayerhosen2345@gmail.com>
2025-07-30 18:41:11 +0600

    new changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* navbar */}
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    const navItems = [
+        <>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/about">About</Link>
+            </li>
+            <li>
+                <Link href="/services">Services</Link>
+            </li>
+            <li>
+                <Link href="/blogs">Blogs</Link>
+            </li>
+            <li>
+                <Link href="/contact">Contact</Link>
+            </li>
+        </>
+    ];
+
+    // note: main ui
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        {navItems}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navItems}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
+        </div>
+    );
+};
+export default Navbar;
